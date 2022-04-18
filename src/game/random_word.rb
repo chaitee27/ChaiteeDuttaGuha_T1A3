3cc7fb433965f5ff40350fea44713dc6fedd7300
chaitee27 <11967@coderacademy.edu.au>
2022-04-19 07:32:39 +1000

    Added my project

--- src/game/random_word.rb
+++ src/game/random_word.rb
@@ -1,6 +1,6 @@
 class RandomWord
   def initialize
-    @words = ["avocado", "chocolate", "dolphin", "plant", "window", "glasses", "onomatopoeia", "babylon", "laptop", "ruby", "blackberry", "street", "zebra", "crossing", "music", "shoes", "south", "bank", "zyzzyva", "quizzing", "bamboozle", "jambeaux", "flapjack", "blizzard", "frazzled", "quincunx", "quadplex", "jezebel", "maximise", "quillaja"]
+    @words = ["argentina", "chocolate", "dolphin", "plant", "window", "glasses", "brazil", "lion", "laptop", "html", "css", "java", "javascript", "crossing", "music", "shoes", "south", "bank", "salsa", "quizzing","hip-hop","bachata","aster","camellias" ,"rock n roll", "travel", "ballet", "blackcurrant", "nactarines", "macadamia", "hungary", "finland", "denmark", "jam"]
   end
 
   def pick_random_word
